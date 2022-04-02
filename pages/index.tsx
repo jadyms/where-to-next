5b0159ad545a345e4efc0026339277c94d7b69d6
jadyms <jady.ms@outlook.com>
2022-04-02 14:42:39 +0100

    chore: api call for list of countries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,51 @@
 import type { GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
+import { ScriptProps } from "next/script";
+import { ReactNode } from "react";
 
-function Home({ countries }) {
+export type Country = {
+  readonly commonName: string;
+  readonly population: number;
+  readonly flag: ReactNode;
+};
+
+export type CountriesProps = {
+  countries: readonly Country[];
+};
+function Home({ countries }: CountriesProps) {
   const { isFallback } = useRouter();
+
+  const onClick = (country: string) => {
+    console.log(country);
+  };
 
   if (isFallback) {
     return "Loading ...";
   }
+
   return (
     <>
-      <h1>Hello</h1>
       <div
         style={{
           display: "grid",
-
           gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
         }}
       >
-        {countries
-          .sort((a, b) =>
-            a.name.common < b.name.common
-              ? -1
-              : a.name.common > b.name.common
-              ? 1
-              : 0
-          )
-          .map((country) => (
-            <div key={country.cioc}>
-              <h1>{country.name.common}</h1>
+        {countries?.map((country: Country) => (
+          <div
+            key={country.commonName}
+            style={{ width: "100%", height: "100%" }}
+          >
+            <button
+              onClick={() => onClick(country.commonName)}
+              style={{ width: "100%", height: "100%", cursor: "pointer" }}
+            >
+              <h1>{country.commonName}</h1>
               <p>{country.population}</p>
               <p>{country.flag}</p>
-            </div>
-          ))}
+            </button>
+          </div>
+        ))}
       </div>
     </>
   );
@@ -43,9 +57,18 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
 
+  const allCountries: Country[] = data.map((c) => {
+    return {
+      commonName: c.name.common,
+      flag: c.flag,
+      population: c.population,
+    };
+  });
+
+  const countries = JSON.parse(JSON.stringify(allCountries));
   return {
     props: {
-      countries: data,
+      countries,
     },
   };
 };
