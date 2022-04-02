a288a55abe2444490330c456bfe232e1fdf453c9
jadyms <jady.ms@outlook.com>
2022-04-02 13:12:16 +0100

    chore: initial api call test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,51 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/router";
 
-const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
-};
+function Home({ countries }) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return "Loading ...";
+  }
+  return (
+    <>
+      <h1>Hello</h1>
+      <div
+        style={{
+          display: "grid",
+
+          gridTemplateColumns: "repeat(4, minmax(0, 1fr))",
+        }}
+      >
+        {countries
+          .sort((a, b) =>
+            a.name.common < b.name.common
+              ? -1
+              : a.name.common > b.name.common
+              ? 1
+              : 0
+          )
+          .map((country) => (
+            <div key={country.cioc}>
+              <h1>{country.name.common}</h1>
+              <p>{country.population}</p>
+              <p>{country.flag}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+
+  return {
+    props: {
+      countries: data,
+    },
+  };
+};
