c9f58d3f462c92045bff761d2bb71f2d1f7b49a8
jadyms <jady.ms@outlook.com>
2022-04-06 13:14:03 +0100

    chore: create theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
+const theme = {
+  colors: {
+    yellow: "#ffa822",
+    blue: "#134e6f",
+    orange: "#ff6150",
+    green: "#1ac0c6",
+    gray: "#f3f4f6",
+    darkGray: "#4b5563",
+    dark: "#111827",
+    white: "#fff",
+  },
+};
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
